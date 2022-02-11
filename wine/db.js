032515eb73fc5e9e15c7ab924bad2a439d76207d
Christina Harris <christinamharris83@gmail.com>
2022-02-11 03:41:30 -0800

    wine db helper functions created, insertWine, UpdateWineProfile, removeWine

--- wine/db.js
+++ wine/db.js
@@ -3,7 +3,23 @@ const db = require("../config/dbConfig");
 module.exports = {
   getAll,
   getById,
+  insertWine,
+  updateWineProfile,
+  removeWine
 };
+
+
+function insertWine(wine) {
+  return db("wine").insert(wine);
+}
+
+function updateWineProfile(id, changes) {
+  return db("wine").where({ id }).update(changes);
+}
+
+function removeWine(id) {
+  return db("wine").where({ id }).del();
+}
 
 function getAll() {
   return db("wine");
