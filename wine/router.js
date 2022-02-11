396eca3aedcfda65e0928674b54a73fb98504b3c
Christina Harris <christinamharris83@gmail.com>
2022-02-11 04:25:08 -0800

    Wine Creation, Wine deletion, and Wine Edit functionalities created. Routes need to be double checked no pgadmin on  this computer

--- wine/router.js
+++ wine/router.js
@@ -1,5 +1,59 @@
 const router = require("express").Router();
 const db = require("./db");
+const {
+  validateLoggedIn,
+} = require("../auth/middleware");
+
+//check
+router.post("/", validateLoggedIn, (req, res) => {
+const wine = req.body
+  db.insertWine(wine)
+    .then((result) => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ error: "error connecting to database" });
+    });
+});
+
+//check
+router.put(
+  "/",
+  validateLoggedIn,
+  (req, res) => {
+    db.updateWineProfile(Number(req.token.subject), req.body)
+      .then((result) => {
+        if (result === 1) {
+          res.status(202).send();
+        } else {
+          res.status(500).json({ error: "Error Updating Wine Profile" });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "error connecting to database" });
+      });
+  }
+);
+
+//check
+router.delete(
+  "/",
+  validateLoggedIn,
+  (req, res) => {
+    db.removeWine(Number(req.token.subject))
+      .then((result) => {
+        if (result === 1) {
+          res.status(202).send();
+        } else {
+          res.status(500).json({ error: "error deleting wine profile" });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "error connecting to database" });
+      });
+  }
+);
 
 router.get("/:id", (req, res) => {
   db.getById(req.params.id)
